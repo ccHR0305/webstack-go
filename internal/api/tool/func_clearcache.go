4405502fdece236d3cf53fa0f9543e79bce3d904
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-11 14:32:12 +0800

    Update func_clearcache.go

--- internal/api/tool/func_clearcache.go
+++ internal/api/tool/func_clearcache.go
@@ -3,9 +3,9 @@ package tool
 import (
 	"net/http"
 
-	"github.com/ch3nnn/webstack-go/internal/code"
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/repository/redis"
+	"github.com/ccHR0305/webstack-go/internal/code"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/repository/redis"
 )
 
 type clearCacheRequest struct {
