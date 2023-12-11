f91b4d2e4dbc044f014a8e288b7a1e2949ea64f9
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-11 14:13:23 +0800

    Update service_listaction.go

--- internal/services/menu/service_listaction.go
+++ internal/services/menu/service_listaction.go
@@ -1,9 +1,9 @@
 package menu
 
 import (
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/model"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/query"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/model"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/query"
 )
 
 type SearchListActionData struct {
