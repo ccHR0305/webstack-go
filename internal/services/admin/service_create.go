e1f5c38078ad4d0edffce797338c0fc0b0524bb5
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-11 14:09:35 +0800

    Update service_create.go

--- internal/services/admin/service_create.go
+++ internal/services/admin/service_create.go
@@ -1,10 +1,10 @@
 package admin
 
 import (
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/pkg/password"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/model"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/query"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/pkg/password"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/model"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/query"
 )
 
 type CreateAdminData struct {
