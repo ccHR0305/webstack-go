a3bb42ea0acab0486a9243e21169ce0756b8e60c
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-14 01:03:38 +0800

    Update func_create.go

--- internal/api/cron/func_create.go
+++ internal/api/cron/func_create.go
@@ -3,10 +3,10 @@ package cron
 import (
 	"net/http"
 
-	"github.com/ch3nnn/webstack-go/internal/code"
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/pkg/validation"
-	"github.com/ch3nnn/webstack-go/internal/services/cron"
+	"github.com/ccHR0305/webstack-go/internal/code"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/pkg/validation"
+	"github.com/ccHR0305/webstack-go/internal/services/cron"
 )
 
 type createRequest struct {
