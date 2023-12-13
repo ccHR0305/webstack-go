96f2cde0ca5f954ec427d966655fb0bbf80e3ffb
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-14 00:52:17 +0800

    Update admin.gen.go

--- internal/repository/mysql/query/admin.gen.go
+++ internal/repository/mysql/query/admin.gen.go
@@ -16,7 +16,7 @@ import (
 
 	"gorm.io/plugin/dbresolver"
 
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/model"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/model"
 )
 
 func newAdmin(db *gorm.DB, opts ...gen.DOOption) admin {
