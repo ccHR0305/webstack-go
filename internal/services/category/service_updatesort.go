0e76032f36f5c5ec19b2bd2d2aa090bf2a560c55
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-13 22:16:23 +0800

    Update service_updatesort.go

--- internal/services/category/service_updatesort.go
+++ internal/services/category/service_updatesort.go
@@ -1,8 +1,8 @@
 package category
 
 import (
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/query"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/query"
 )
 
 func (s *service) UpdateSort(ctx core.Context, id, sort int64) (err error) {
