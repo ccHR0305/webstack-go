3f09e1cd35b453dafb51600ceac19a6eed9c0976
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-14 00:35:19 +0800

    Update service_delete.go

--- internal/services/menu/service_delete.go
+++ internal/services/menu/service_delete.go
@@ -1,8 +1,8 @@
 package menu
 
 import (
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/query"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/query"
 )
 
 func (s *service) Delete(ctx core.Context, id int64) (err error) {
