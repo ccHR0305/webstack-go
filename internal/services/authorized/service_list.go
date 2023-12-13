c1214681a2f13718725785762daf9493b96cd8c8
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-14 00:41:34 +0800

    Update service_list.go

--- internal/services/authorized/service_list.go
+++ internal/services/authorized/service_list.go
@@ -1,9 +1,9 @@
 package authorized
 
 import (
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/model"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/query"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/model"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/query"
 )
 
 func (s *service) List(ctx core.Context, searchData *SearchData) (authorizedList []*model.Authorized, err error) {
