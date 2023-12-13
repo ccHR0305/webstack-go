f7f0750870aa49de1b2e2c87077b21a87a522e89
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-14 01:01:11 +0800

    Update func_create.go

--- internal/api/authorized/func_create.go
+++ internal/api/authorized/func_create.go
@@ -3,9 +3,9 @@ package authorized
 import (
 	"net/http"
 
-	"github.com/ch3nnn/webstack-go/internal/code"
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/services/authorized"
+	"github.com/ccHR0305/webstack-go/internal/code"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/services/authorized"
 )
 
 type createRequest struct {
