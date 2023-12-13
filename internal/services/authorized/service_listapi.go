d4efaf6f317e48b16eaad0eb9da4656d579a7abd
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-13 22:19:52 +0800

    Update service_listapi.go

--- internal/services/authorized/service_listapi.go
+++ internal/services/authorized/service_listapi.go
@@ -1,9 +1,9 @@
 package authorized
 
 import (
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/model"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql/query"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/model"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql/query"
 )
 
 type SearchAPIData struct {
