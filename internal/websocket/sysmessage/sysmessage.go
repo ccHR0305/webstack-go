94d141d712a31e5147c60056691b415ad3b96008
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-13 22:36:25 +0800

    Update sysmessage.go

--- internal/websocket/sysmessage/sysmessage.go
+++ internal/websocket/sysmessage/sysmessage.go
@@ -1,11 +1,11 @@
 package sysmessage
 
 import (
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/pkg/errors"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql"
-	"github.com/ch3nnn/webstack-go/internal/repository/redis"
-	"github.com/ch3nnn/webstack-go/internal/repository/socket"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/pkg/errors"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql"
+	"github.com/ccHR0305/webstack-go/internal/repository/redis"
+	"github.com/ccHR0305/webstack-go/internal/repository/socket"
 
 	"go.uber.org/zap"
 )
