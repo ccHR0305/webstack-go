58eaf5c176decf516e473f2d3f1ba3493f42c9ad
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-14 01:04:11 +0800

    Update func_md5.go

--- internal/api/helper/func_md5.go
+++ internal/api/helper/func_md5.go
@@ -5,8 +5,8 @@ import (
 	"encoding/hex"
 	"net/http"
 
-	"github.com/ch3nnn/webstack-go/internal/code"
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/code"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
 )
 
 type md5Request struct {
