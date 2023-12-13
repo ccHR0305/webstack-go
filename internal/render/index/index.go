c69ca7ccbcae37f2268ae51a157dad1b6e1525e4
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-14 01:09:45 +0800

    Update index.go

--- internal/render/index/index.go
+++ internal/render/index/index.go
@@ -1,9 +1,9 @@
 package index
 
 import (
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql"
-	"github.com/ch3nnn/webstack-go/internal/repository/redis"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql"
+	"github.com/ccHR0305/webstack-go/internal/repository/redis"
 
 	"go.uber.org/zap"
 )
