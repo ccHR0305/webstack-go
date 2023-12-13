26f2293197296952d08e6065d06015762fd6bd49
ccHR0305 <49118433+ccHR0305@users.noreply.github.com>
2023-12-13 22:35:10 +0800

    Update config.go

--- internal/render/config/config.go
+++ internal/render/config/config.go
@@ -4,11 +4,11 @@ import (
 	"go/token"
 	"log"
 
-	"github.com/ch3nnn/webstack-go/configs"
-	"github.com/ch3nnn/webstack-go/internal/code"
-	"github.com/ch3nnn/webstack-go/internal/pkg/core"
-	"github.com/ch3nnn/webstack-go/internal/repository/mysql"
-	"github.com/ch3nnn/webstack-go/internal/repository/redis"
+	"github.com/ccHR0305/webstack-go/configs"
+	"github.com/ccHR0305/webstack-go/internal/code"
+	"github.com/ccHR0305/webstack-go/internal/pkg/core"
+	"github.com/ccHR0305/webstack-go/internal/repository/mysql"
+	"github.com/ccHR0305/webstack-go/internal/repository/redis"
 
 	"github.com/dave/dst"
 	"github.com/dave/dst/decorator"
